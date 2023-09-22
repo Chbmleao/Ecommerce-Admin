6b804d176f6cfdbfa02b2cd915b4df9776444f0c
Carlos Leão <caikleao@gmail.com>
2023-09-22 12:39:43 -0300

    refactor: create layout component file

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,25 @@
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
+
+import Layout from "@/components/Layout";
 
 export default function Home() {
   const { data: session } = useSession();
-  if (!session) {
-    return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button
-            onClick={() => signIn("google")}
-            className="bg-white p-2 px-4 rounded-lg"
-          >
-            Login with Google
-          </button>
+
+  return (
+    <Layout>
+      <div className="text-blue-900 flex justify-between">
+        <h2>
+          Hello, <b>{session?.user?.name}</b>
+        </h2>
+        <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden items-center">
+          <img
+            src={session?.user?.image}
+            alt="Profile Picture"
+            className="w-8 h-8"
+          />
+          <span className="px-2">{session?.user?.name}</span>
         </div>
       </div>
-    );
-  }
-
-  return <div>logged in {session.user.email}</div>;
+    </Layout>
+  );
 }
