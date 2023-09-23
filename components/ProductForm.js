4213311ea632a73f9dc063d6087f6d5be95b1041
Carlos Leão <caikleao@gmail.com>
2023-09-23 12:41:10 -0300

    feat: add methods to upload and visualize images

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -7,15 +7,18 @@ export default function ProductForm({
   title: existingTitle,
   description: existingDescription,
   price: existingPrice,
+  images: existingImages,
 }) {
   const [title, setTitle] = useState(existingTitle || "");
   const [description, setDescription] = useState(existingDescription || "");
   const [price, setPrice] = useState(existingPrice || "");
+  const [images, setImages] = useState(existingImages || []);
   const router = useRouter();
+
   async function saveProduct(ev) {
     ev.preventDefault();
 
-    const data = { title, description, price };
+    const data = { title, description, price, images };
     if (_id) {
       // update product
       await axios.put("/api/products", { ...data, _id });
@@ -27,6 +30,20 @@ export default function ProductForm({
     router.push("/products");
   }
 
+  async function uploadImages(ev) {
+    const files = ev.target?.files;
+    if (files?.length > 0) {
+      const data = new FormData();
+      for (const file of files) {
+        data.append("file", file);
+      }
+      const response = await axios.post("/api/upload", data);
+      setImages((oldImages) => {
+        return [...oldImages, ...response.data];
+      });
+    }
+  }
+
   return (
     <form onSubmit={saveProduct}>
       <label>Product name</label>
@@ -36,6 +53,33 @@ export default function ProductForm({
         value={title}
         onChange={(ev) => setTitle(ev.target.value)}
       />
+      <label>Photos</label>
+      <div className="mb-2 flex flex-wrap gap-2">
+        {images &&
+          images.map((link) => (
+            <div key={link} className="h-24">
+              <img className="rounded-lg" src={link} alt="Product image" />
+            </div>
+          ))}
+        <label className="w-32 h-32 flex items-center  flex-col justify-center gap-1 text-gray-500 rounded-lg bg-gray-200 text-sm cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-7 h-7"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15"
+            />
+          </svg>
+          <div>Upload</div>
+          <input type="file" onChange={uploadImages} className="hidden" />
+        </label>
+      </div>
       <label>Description</label>
       <textarea
         placeholder="description"
