5d7642410c7dc99640a3099bcc37a1f6a17406de
Carlos Leão <caikleao@gmail.com>
2023-09-24 13:49:22 -0300

    feat: add categories get method

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -1,7 +1,14 @@
+import { mongooseConnect } from "@/lib/mongoose";
 import { Category } from "@/models/Category";
 
 export default async function handle(req, res) {
+  await mongooseConnect();
   const { method } = req;
+
+  if (method === "GET") {
+    const categories = await Category.find();
+    res.json(categories);
+  }
 
   if (method === "POST") {
     const { name } = req.body;
