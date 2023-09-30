5dfcbed165f13832e0b88a370379127416079ae8
Carlos Leão <caikleao@gmail.com>
2023-09-30 10:46:04 -0300

    feat: change layout style sheet

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,14 @@
 import Navbar from "@/components/Navbar";
 import { useSession, signIn } from "next-auth/react";
+import { useState } from "react";
+import Logo from "./Logo";
 
 export default function Layout({ children }) {
   const { data: session } = useSession();
+  const [showNav, setShowNav] = useState(false);
   if (!session) {
     return (
-      <div className="bg-indigo-950 w-screen h-screen flex items-center">
+      <div className="bg-secondary-100 w-screen h-screen flex items-center">
         <div className="text-center w-full">
           <button
             onClick={() => signIn("google")}
@@ -19,10 +22,27 @@ export default function Layout({ children }) {
   }
 
   return (
-    <div className="bg-indigo-950 min-h-screen flex">
-      <Navbar />
-      <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">
-        {children}
+    <div className="bg-secondary-100">
+      <div className="md:hidden flex items-center justify-between p-4 pb-2">
+        <Logo />
+        <button onClick={() => setShowNav(true)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+      </div>
+      <div className="min-h-screen flex">
+        <Navbar show={showNav} />
+        <div className="flex-grow p-4">{children}</div>
       </div>
     </div>
   );
